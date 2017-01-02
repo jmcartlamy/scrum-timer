75f9e1d27a039905b3bf3e40d9ee91086df4d049
Jean-Marie Cart-Lamy <jean-marie.cart-lamy@fullsix.com>
2017-01-02 14:21:51 +0100

    Clean Example

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,13 @@
 import React, {Component} from 'react';
 
 import ExampleComponent from '../components/ExampleComponent.js'
-import logo from '../../assets/logo.svg';
 
 class App extends Component {
 
   render() {
 
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h2>Welcome to React</h2>
-        </div>
+      <div className="Scrum-App">
         <ExampleComponent />
       </div>
     );
