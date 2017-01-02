b89be51ec02bffef8f6d96cc4083a8ced34f1dd1
Jean-Marie Cart-Lamy <jean-marie.cart-lamy@fullsix.com>
2017-01-02 11:39:43 +0100

    Add example reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+/*
+  Maybe combineReducers for multiple reducers :
+  import { combineReducers } from 'redux';
+
+  Import yours reducers. Example :
+  import userInputReducer from './userInput.js';
+ */
+
+/*
+  export default combineReducers({ userInputReducer });
+  OR
+  export default userInputReducer
+ */
