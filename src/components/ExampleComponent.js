2707dfae7520907b384063e8d08d8915ec40802f
Jean-Marie Cart-Lamy <jean-marie.cart-lamy@fullsix.com>
2017-01-02 11:39:22 +0100

    Add Example Component

---
+++ src/components/ExampleComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ExampleComponent extends Component {
+
+  render() {
+    return (
+      <p className="App-intro">
+        To get started, edit <code>src/containers/App.js</code> and save to reload.
+      </p>
+    )
+  }
+}
+
+export default ExampleComponent
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+import ExampleComponent from '../components/ExampleComponent.js'
 import logo from '../../assets/logo.svg';
 
 class App extends Component {
+
   render() {
+
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/containers/App.js</code> and save to reload.
-        </p>
+        <ExampleComponent />
       </div>
     );
+
   }
 }
 
